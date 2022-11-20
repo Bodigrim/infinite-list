2dd46ba53f0efbd3b2ebaa5578894232dd613b47
BlackCap <mailhph@gmail.com>
2022-11-20 03:45:41 +0100

    uninterleaveR in terms of uninterleaveL

--- src/Data/List/Infinite.hs
+++ src/Data/List/Infinite.hs
@@ -416,7 +416,7 @@ uninterleaveL :: Infinite a -> Infinite a
 uninterleaveL (l :< ~(_ :< ls)) = l :< uninterleaveL ls
 
 uninterleaveR :: Infinite a -> Infinite a
-uninterleaveR (_ :< r :< rs) = r :< uninterleaveR rs
+uninterleaveR s = uninterleaveL (tail s)
 
 -- | Insert an element between adjacent elements of an infinite list.
 intersperse :: a -> Infinite a -> Infinite a
